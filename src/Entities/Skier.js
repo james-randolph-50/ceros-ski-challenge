77e3956c2f481cfe09c1881dca8c0b4bece869bb
Mary Kate <MaryKate@Marys-MBP-3.lan1>
2019-06-19 16:13:06 -0400

    jumping over everything

--- src/Entities/Skier.js
+++ src/Entities/Skier.js
@@ -129,9 +129,10 @@ export class Skier extends Entity {
             return intersectTwoRects(skierBounds, obstacleBounds);
         });
 
-        if(collision) {
-            this.setDirection(Constants.SKIER_DIRECTIONS.CRASH);
-
+        if (this.direction != Constants.SKIER_DIRECTIONS.JUMP_ONE && this.assetName != "rock" ) {
+            if(collision) {
+                this.setDirection(Constants.SKIER_DIRECTIONS.CRASH);
+            }
         }
     };
      
